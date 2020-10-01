57590873bc1f2727e02ec28c8b998e9fb38e1875
Alexandra K <64307537+aliks-ray@users.noreply.github.com>
2020-10-01 13:25:40 +0300

    Внесены исправления после проверки

--- js/stat.js
+++ js/stat.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const CANVAS_HEIGHT = 270;
-const CANVAS_WIDTH = 420;
-const CANVAS_X = 100;
-const CANVAS_Y = 10;
-const CANVAS_Y_BOTTOM = CANVAS_Y + CANVAS_HEIGHT;
-const CANVAS_GAP = 10;
+const CLOUD_HEIGHT = 270;
+const CLOUD_WIDTH = 420;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const CLOUD_Y_BOTTOM = CLOUD_Y + CLOUD_HEIGHT;
+const CLOUD_GAP = 10;
 const CONTENT_GAP = 20;
-const CANVAS_FONT = 16;
+const CLOUD_FONT = 16;
 const BAR_HEIGHT = 150;
 const BAR_WIDTH = 40;
 const BAR_GAP = 50;
 
 
-let renderCloud = function (ctx, x, y, color) {
+const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 const getMaxElement = function (arr) {
@@ -38,8 +38,8 @@ const renderPlayersResults = function (ctx, names, times) {
 
   for (let i = 0; i < names.length; i++) {
     let currentBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    let currentX = CANVAS_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
-    let currentY = CANVAS_Y_BOTTOM - CONTENT_GAP;
+    let currentX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    let currentY = CLOUD_Y_BOTTOM - CONTENT_GAP;
     let currentTime = Math.round(times[i]);
 
     ctx.fillStyle = `#000000`;
@@ -51,47 +51,48 @@ const renderPlayersResults = function (ctx, names, times) {
     ctx.fillText(
         currentTime,
         currentX,
-        currentY - CANVAS_FONT - currentBarHeight - CANVAS_GAP
+        currentY - CLOUD_FONT - currentBarHeight - CONTENT_GAP
     );
 
-    ctx.fillStyle = (names[i] === `Вы`) ? `rgba(255, 0, 0, 1)` : `hsl(230, 50%, ${randomInt(100)}%)`;
+    ctx.fillStyle = (names[i] === `Вы`) ? `rgba(255, 0, 0, 1)` : `hsl(230, ${randomInt(100)}%, 50%)`;
 
     ctx.fillRect(
         currentX,
-        currentY - CANVAS_FONT,
+        currentY - CLOUD_FONT,
         BAR_WIDTH,
         currentBarHeight * (-1)
     );
   }
 };
 
+function renderText(canv, x, y, text) {
+  const strs = text.split(`\n`);
+  let currentY = y;
+
+  canv.fillStyle = `#000000`;
+  canv.font = `16px PT Mono`;
+  canv.textBaseline = `hanging`;
+
+  strs.forEach((str) => {
+    canv.fillText(str, x, currentY);
+    currentY += CLOUD_FONT + CLOUD_GAP;
+  });
+}
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(
       ctx,
-      CANVAS_X + CANVAS_GAP,
-      CANVAS_Y + CANVAS_GAP,
+      CLOUD_X + CLOUD_GAP,
+      CLOUD_Y + CLOUD_GAP,
       `rgba(0, 0, 0, 0.7)`
   );
   renderCloud(
       ctx,
-      CANVAS_X,
-      CANVAS_Y,
+      CLOUD_X,
+      CLOUD_Y,
       `#ffffff`
   );
 
-  ctx.fillStyle = `#000000`;
-  ctx.font = `16px PT Mono`;
-  ctx.fillText(
-      `Ура вы победили!`,
-      CANVAS_X + CONTENT_GAP,
-      CANVAS_Y + CONTENT_GAP + CANVAS_FONT
-  );
-  ctx.fillText(
-      `Список результатов: `,
-      CANVAS_X + CONTENT_GAP,
-      CANVAS_Y + CONTENT_GAP + CANVAS_FONT * 2
-  );
-
+  renderText(ctx, CLOUD_X + CONTENT_GAP, CLOUD_Y + CONTENT_GAP, `Ура вы победили! \nСписок результатов:`);
   renderPlayersResults(ctx, names, times);
 };
